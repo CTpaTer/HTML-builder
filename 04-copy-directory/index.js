4ad7ac760ed80d260daaa1627cd3c110044c0c04
Pavel Demaev <ctpater@gmail.com>
2022-11-02 18:41:20 +0300

    feat: done task 04

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,29 @@
+const path = require('path');
+const fs = require('fs');
+const src = path.resolve(__dirname, './files');
+const dst = path.resolve(__dirname, './files-copy');
 
+// remove directory
+fs.rm(dst, { recursive: true, force: true }, (err) => {
+    if (err) throw err;
+
+    // create directory
+    fs.mkdir(dst, { recursive: true }, (err) => {
+        if (err) throw err;
+    });
+
+    // copy files
+    fs.readdir(src, {withFileTypes: true}, (err, files) => {
+        if (err) {
+            console.log(err);
+        } else {
+            files.forEach(file => {
+                if (file.isFile()) {
+                    fs.copyFile(path.join(src, file.name), path.join(dst, file.name), (err) => {
+                        if (err) throw err;
+                        })
+                }
+            })
+        }
+    })
+});
