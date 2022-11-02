fc984fc64a4b08e1fc8731facbc5076c7a843617
Pavel Demaev <ctpater@gmail.com>
2022-11-02 13:19:23 +0300

    fix: add error message to  task 01

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -6,3 +6,4 @@ const streamPath = path.resolve(__dirname, 'text.txt');
 
 const readableStream = fs.createReadStream(streamPath, 'utf-8');
 readableStream.on('data', chunk => stdout.write(chunk));
+readableStream.on('error', error => console.log('Error', error.message));
