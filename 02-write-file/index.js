ad92290d5002ec1d33c7432220a052466afc384a
Pavel Demaev <ctpater@gmail.com>
2022-11-02 14:47:22 +0300

    feat: done task 02

---
+++ 02-write-file/index.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const fs = require('fs');
+const { stdin, stdout } = process;
+const streamPath = path.resolve(__dirname, 'text.txt');
+
+const output = fs.createWriteStream(streamPath);
+
+stdout.write('Please, typing your text:\n')
+stdin.on('data', data => {
+    const text = data.toString().slice(0, -1);
+    if(text !== 'exit') {
+        output.write(text + '\n');
+    } else {
+        process.exit();
+    }
+});
+process.on('exit', () => stdout.write('Have a nice day!\n'));
+process.on('SIGINT', () => process.exit());
+
+stdin.on('error', error => console.log('Error', error.message));
